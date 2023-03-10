52f7f8c352e4c63cb86788b97aeabd0ff3b9670f
Brad <bradlowe@g.ucla.edu>
2023-03-09 19:00:57 -0800

    create test interactable

---
+++ Shopkeeper/Assets/Scripts/Map Scripts/InteractableSceneSwitch.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableSceneSwitch : MonoBehaviour, Interactable
+{
+    GameObject player;
+
+    void Start() {
+        player = GameObject.Find("Player");
+    }
+    public void Interact() {
+        Debug.Log("switch scene");
+    }
+
+    void Update() {
+        float dist = (transform.position - player.transform.position).magnitude;
+        //Debug.Log(dist);
+        if(dist < 1) {
+            Interact();
+        }
+    }
+}
