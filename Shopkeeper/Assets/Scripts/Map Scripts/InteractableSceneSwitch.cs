295080bc97ce83c162d4dd627ca93a93a5603cd7
Joaquin Cruz <cruzjoaquin689@gmail.com>
2023-03-10 00:11:22 -0800

    new changes

--- Shopkeeper/Assets/Scripts/Map Scripts/InteractableSceneSwitch.cs
+++ Shopkeeper/Assets/Scripts/Map Scripts/InteractableSceneSwitch.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
+using UnityEngine.SceneManagement;
 
-public class InteractableSceneSwitch : MonoBehaviour, Interactable
-{
-    GameObject player;
+public class InteractableSceneSwitch : MonoBehaviour, Interactable 
+{ 
+	GameObject player; 
+	[SerializedField] string sceneName; 
+    /*
+        SerializedField will allow to make the sceneName variable editable in Unity 
+    */
 
-    void Start() {
-        player = GameObject.Find("Player");
-    }
-    public void Interact() {
-        Debug.Log("switch scene");
-    }
+	void Start() {
+		player = GameObject.Find("Player"); 
 
-    void Update() {
-        float dist = (transform.position - player.transform.position).magnitude;
-        Debug.Log(dist);
+	}
+	public void Interact () {
+		SceneManager.LoadScene(sceneName); 
+	}
+	void Update() {
+		float dist = (transform.position - player.transform.position).magnitude;
 		if(dist < 1) {
 			Interact(); 
 		}
